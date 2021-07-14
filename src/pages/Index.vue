876b1b2be817a57993a81d9a32d9eb21224b78ed
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-07-14 09:46:02 +0200

    cleanup

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,24 +1,18 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-    <div class="q-pa-md">
+  <q-page class="flex flex-center  test">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+      class="fixed-center light-dimmed"
+      style="opacity: 0.2"
+    />
+
+    <div class="q-pa-md z-top   ">
       <q-infinite-scroll @load="onLoad" :offset="250">
-        <div v-for="(item, index) in items" :key="index" class="caption">
-          <p>
-            {{ index }}
-            {{ item }}
-            <!-- {{ items }} -->
-            <!-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-            repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis
-            perferendis totam, ea at omnis vel numquam exercitationem aut, natus
-            minima, porro labore. -->
-          </p>
+        <div v-for="(item, index) in items" :key="index" class="text-h4">
+          <p>{{ item }}</p>
+          <br />
         </div>
-        <template v-slot:loading>
-          <div class="row justify-center q-my-md">
-            <q-spinner-dots color="primary" size="40px" />
-          </div>
-        </template>
       </q-infinite-scroll>
     </div>
   </q-page>
@@ -28,19 +22,30 @@
 export default {
   data() {
     return {
-      items: [{}, {}, {}, {}, {}, {}, {}]
+      items: [{}],
+      timerId: null
     };
   },
 
   methods: {
+    pageScroll() {
+      window.scrollBy(0, 1);
+    },
     onLoad(index, done) {
-      setTimeout(() => {
-        if (this.items) {
-          this.items.push("das", "ist", "das", "hinzugefügte", "element");
-          done();
-        }
-      }, 0);
+      console.log(index);
+      if (this.items) {
+        this.items.push("1 - one", "2 - two", "3 - three");
+        done();
+      }
     }
+  },
+  mounted() {
+    let timerId = setInterval(() => this.pageScroll(), 22);
+  },
+  destroyed() {
+    clearInterval(this.timerId);
   }
 };
 </script>
+
+<style></style>
